1c7e2d2fdaafccac8b27640ea56d639e0ba1b7aa
Darko Tasevski <c.dtasevski@gogoair.com>
2018-04-12 19:03:19 +0200

    Add particles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+
 import './App.css';
+
 import Nav from './components/Nav/Nav';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -7,10 +10,53 @@ import ImgForm from './components/ImgForm/ImgForm';
 
 const FaceRecognition = () => <p>Test Component</p>;
 
+const particlesConfig = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: '#3CA9D1',
+        blur: 5
+      }
+    },
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse'
+      },
+      onclick: {
+        enable: true,
+        mode: 'push'
+      },
+      resize: true
+    },
+    modes: {
+      repulse: {
+        distance: 100,
+        duration: 0.2
+      },
+      push: {
+        particles_nb: 4
+      }
+    }
+  }
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesConfig} />
         <Nav />
         <Logo />
         <Rank />
